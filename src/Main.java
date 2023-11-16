a3e7f49c9c39f9d48b865b1099e0647c19a717de
huhdy32 <mong3268@gmail.com>
2023-11-16 20:48:44 +0900

    fix: jpegReader 수정

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,12 @@ public class Main {
         ImageReader reader = null;
         String image = args[0];
         String format = image.substring(image.indexOf('.') + 1, (image.length()));
+
         if (format.equals("gif")) {
             reader = new GifReader(image);
         }
         if (format.equals("jpeg")) {
-            reader = new GifReader(image);
+            reader = new JpegReader(image);
         }
         assert reader != null;
         decodedImage = reader.getDecodeImage();
